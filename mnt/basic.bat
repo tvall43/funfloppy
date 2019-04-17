1c8785209c73cb369df38060c5f4a6d18dcfd1ac
tim <tvall43@gmail.com>
2019-04-17 09:15:44 -0500

    v2, some changes. zork compressed. basic and invaders added

---
+++ mnt/basic.bat
@@ -0,0 +1,8 @@
+xmsdsk 29576 T: /y
+mkdir T:\temp
+pkunzip -e \xz.zip T:\temp\
+T:\temp\xz -d -c \bwbasic.xz > T:\temp\bwbasic.zip
+pkunzip -e -d T:\temp\bwbasic.zip T:\
+T:
+bwbasic
+
--- mnt/zork.bat
+++ mnt/zork.bat
@@ -1,2 +1,8 @@
+xmsdsk 29576 S: /y
+mkdir S:\temp
+pkunzip -e \xz.zip S:\temp\
+S:\temp\xz -d -c \zork1.xz > S:\temp\zork1.zip
+pkunzip -e -d S:\temp\zork1.zip S:\
+S:
 cd zork1
 zork1
