fab7c7241a29569907210f8b7fc6cf0c016d32df
tim <tvall43@gmail.com>
2019-04-17 09:17:40 -0500

    andventure added, grub compressed. updated freedos

---
+++ mnt/advent.bat
@@ -0,0 +1,6 @@
+xmsdsk 576 P: /y
+mkdir P:\temp
+pkunzip -e \xz.zip P:\temp\
+P:\temp\xz -d -c \advent.xz > P:\advent.exe 
+P:
+advent
--- mnt/boot.bat
+++ mnt/boot.bat
@@ -1,1 +1,6 @@
-GRUB --config-file="chainloader (hd0)+1;boot"
+xmsdsk 576 U: /y
+mkdir U:\temp
+pkunzip -e \xz.zip U:\temp\
+U:\temp\xz -d -c \grub.xz > U:\grub.exe 
+u:
+GRUB --config-file="chainloader (hd0)+1"
